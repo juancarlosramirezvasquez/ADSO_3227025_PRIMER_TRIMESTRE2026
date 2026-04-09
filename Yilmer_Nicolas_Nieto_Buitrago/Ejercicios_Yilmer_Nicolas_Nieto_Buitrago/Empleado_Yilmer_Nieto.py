4d825f8b5dd8a9d4a2aa661caf5979cf2ec27455
Yilmer <yilmernicolasnietobuitrago@gmail.com>
2026-04-09 15:43:01 -0500

    Add files via upload

---
+++ Yilmer_Nicolas_Nieto_Buitrago/Ejercicios_Yilmer_Nicolas_Nieto_Buitrago/Empleado_Yilmer_Nieto.py
@@ -0,0 +1,20 @@
+class Empleado():
+    def __init__(self, nombre, salario):
+        self.nombre = nombre
+        self.salario = salario
+        
+class Gerente(Empleado):
+    def __init__(self, nombre, salario, departamento):
+        super().__init__(nombre, salario)
+        self.departamento = departamento
+    def mostrar_info(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Salario: {self.salario}")
+        print(f"Departamento: {self.departamento}")
+            
+def main():
+    gerente1 = Gerente("Chis", 5000, "Alta tacnologia de desarrollo")
+    gerente1.mostrar_info()
+    
+if __name__ == "__main__":
+    main()
